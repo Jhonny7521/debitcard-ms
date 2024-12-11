25e262c21873b0cf234e42ec5bbcefb5c7143c8e
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2024-12-11 15:28:33 -0500

    :fire: First Commit Microservice debitcard-ms

---
+++ src/main/java/com/bm_nttdata/debitCard_ms/DebitCardMsApplication.java
@@ -0,0 +1,13 @@
+package com.bm_nttdata.debitCard_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitCardMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitCardMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bm_nttdata/debitCard_ms/DebitCardMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bm_nttdata.debitCard_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebitCardMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
